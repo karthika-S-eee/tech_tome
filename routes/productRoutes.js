689da3a3427ed7b5b8de79baed1ba01caf7f73af
Karthika <karthika.s2022eee@sece.ac.in>
2024-08-13 22:35:36 +0530

    changed auth

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const adminAuth=require("../middlewares/adminauth")
 const auth= require('../middlewares/auth');
 
-router.get("/admin/get",adminAuth,ProductControllers.getProducts)
+router.get("/admin/get",ProductControllers.getProducts)
 router.post("/create", adminAuth,ProductControllers.createProduct)
-router.get("/get",auth,ProductControllers.getProducts)
+router.get("/get",ProductControllers.getProducts)
 module.exports= router;
